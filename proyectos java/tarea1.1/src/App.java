bdeb2218affa22060891d44d9eb35cb2337a24ab
Exigur <110357889+Exigur@users.noreply.github.com>
2022-07-31 20:11:29 -0600

    primera tarea para empezar

---
+++ proyectos java/tarea1.1/src/App.java
@@ -0,0 +1,34 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int resultado;
+        resultado = suma(1,2,3);
+     
+        System.out.println(resultado);
+
+        coche nuevocoche= new coche();
+        nuevocoche.AnadirPuerta();
+        nuevocoche.AnadirPuerta();
+        nuevocoche.AnadirPuerta();
+
+
+        System.out.println(nuevocoche.puertas);
+    }
+    public static int suma (int numero1, int numero2,int  numero3) {
+
+        return numero1 + numero2 + numero3;
+    }
+    
+       
+    }
+
+    class coche {
+
+        public int puertas = 4;
+ 
+        public void AnadirPuerta (){
+ 
+                  this.puertas++;
+        }
+
+    
+}
