0cda71d2d9ec545ae7bc9292f544aaf7a708fc90
Exigur <110357889+Exigur@users.noreply.github.com>
2022-08-09 21:13:26 -0600

    Tarea2

---
+++ proyectos java/tarea 2/Tarea2/src/App.java
@@ -0,0 +1,46 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+    
+         int numuerolf = 0;
+
+         if (numuerolf <0) {
+
+          System.out.print ( "el numero es negativo");
+         } 
+         
+         else if (numuerolf>0) {
+
+            System.out.print( "el numero es positivo");
+
+         }
+
+         else {
+
+            System.out.print( "el numero es cero");
+            
+         }
+
+         
+        
+    }
+
+  
+
+    
+
+    
+    }
+
+
+    
+
+    
+
+
+    
+    
+
+
+    
+
